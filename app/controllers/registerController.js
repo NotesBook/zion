d471990f9aeec8cf01566a7bf8f365fc66d66b1c
juabons <juanfmedinar@gmail.com>
2016-05-23 21:39:18 +0200

    #184144

--- app/controllers/registerController.js
+++ app/controllers/registerController.js
@@ -1,6 +1,8 @@
 nbApp.controller('RegisterController', ['$scope','ValidationService','CountriesService','AjaxService',
 	function($scope,ValidationService,CountriesService,AjaxService) { 
 
+        $scope.show_valid_modal;
+
         $scope.form_data = Array();
 
         $scope.form_data = {
@@ -29,8 +31,14 @@ nbApp.controller('RegisterController', ['$scope','ValidationService','CountriesS
          // Send the form input data to process it at backend
          $scope.send_form_data = function() {
 
-            AjaxService.send('POST','api/user/register',JSON.stringify($scope.form_data));
+            AjaxService.send('POST','api/user/register',JSON.stringify($scope.form_data)).then(function(response) {
+                
+                 if(response.valid == true) {
+                    $scope.show_valid_modal = true;
+                 } else $scope.show_valid_modal = false;
 
+            });
          }
+
 
 	}]);
