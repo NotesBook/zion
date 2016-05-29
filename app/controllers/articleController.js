907bb909bcf18f505748c4c236ff130702e61a9e
juabons <juanfmedinar@gmail.com>
2016-05-29 13:53:30 +0200

    Task #165393

--- app/controllers/articleController.js
+++ app/controllers/articleController.js
@@ -2,8 +2,10 @@ nbApp.controller('ArticleController', ['$scope',
 	function($scope) {
 
 		$scope.articles = [
-			'Contenido 1',
-			'Contenido 2',
-		];
+			{ nombre: "Articulo 1", comentario: "Comentarios", contenido: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+			{ nombre:"Articulo 2", comentario: "Comentarios",contenido: "Contenido 2" },
+			{ nombre:"Articulo 3", comentario: "Comentarios",contenido: "Contenido 3" },
+			{ nombre:"Articulo 4", comentario: "Comentarios",contenido: "Contenido 4" },								
+		];	
 
 }])
