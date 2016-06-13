0b46ab8f2dddf5a632b94e81b1fd9f480545f8fa
juabons <juanfmedinar@gmail.com>
2016-06-13 09:38:15 +0200

    task #200075

--- app/controllers/articleController.js
+++ app/controllers/articleController.js
@@ -1,13 +1,6 @@
 nbApp.controller('ArticleController', ['$scope','ArticlesService','ValidationService','SharedDataService','UserService',
 	function($scope,ArticlesService,ValidationService,SharedDataService,UserService) {
 
-		// Get the classroom object recived from dashboard
-        $scope.classroom_item = SharedDataService.get_val();
-
-		// ArticlesService.get_classroom_articles('GET',"api/classroom/last_articles",$scope.classroom_item.id).then(function(response) {
-		// 	$scope.classroom_articles = response.data;
-		// 	});	        
-
 		// Get logged in user data
 		UserService.get_logged_user_data().then(function(response) {
 			$scope.logged_user_data = response.data;
